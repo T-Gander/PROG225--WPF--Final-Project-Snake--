378b0803ee0c9707a7f6317c9794a4d208d50338
Thomas Gander - School <thomas.gander@mymhc.ca>
2023-11-15 11:17:10 -0700

    Added looping background video.

--- PROG225--Final-Project-Snake--/Pages/MainMenu.xaml.cs
+++ PROG225--Final-Project-Snake--/Pages/MainMenu.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace PROG225__Final_Project_Snake__.Pages
 {
@@ -20,9 +23,30 @@ namespace PROG225__Final_Project_Snake__.Pages
     /// </summary>
     public partial class MainMenu : Page
     {
+        static DispatcherTimer videoTimer;
+        int videoCount = 0;
+
         public MainMenu()
         {
             InitializeComponent();
+
+            videoTimer = new DispatcherTimer();
+            videoTimer.Interval = new TimeSpan(0,0,1);
+            videoTimer.Tick += VideoTimer_Tick;
+            videoTimer.Start();
+        }
+
+        private void VideoTimer_Tick(object? sender, EventArgs e)
+        {
+            if(videoCount == 20)
+            {
+                backgroundVideo.Position = new TimeSpan(0,0,0);
+                videoCount = 0;
+            }
+            else
+            videoCount++;
+
+            Debug.WriteLine($"{videoCount}");
         }
     }
 }
