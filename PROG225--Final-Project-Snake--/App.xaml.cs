455766c929cc43a98b8f0f73308ed69f8be167eb
Thomas Gander <Thomas.Gander@mymhc.ca>
2023-11-15 03:49:57 -0800

    Add project files.

---
+++ PROG225--Final-Project-Snake--/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PROG225__Final_Project_Snake__
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
